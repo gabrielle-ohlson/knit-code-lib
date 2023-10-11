f8c4cb827b868df7a905b9166c90e669411210a7
gabrielle-ohlson <gmoa2017@mymail.pomona.edu>
2023-10-11 15:21:52 -0400

    trying to get subdir imports working

--- stitchPatterns.py
+++ stitchPatterns.py
@@ -1,3 +1,7 @@
+# import os
+# import sys
+# sys.path.insert(0, os.getcwd())
+
 from helpers import c2cs, convertToBN, includeNSecureSides, tuckPattern, knitPass
 
 # --------------------------------
