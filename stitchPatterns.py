d4622a24224cc81304f8482305ef5734b3910be7
gabrielle-ohlson <gmoa2017@mymail.pomona.edu>
2023-10-17 19:51:52 -0400

    fixed typo

--- stitchPatterns.py
+++ stitchPatterns.py
@@ -574,7 +574,7 @@ def tuckGarter(k, start_n, end_n, passes, c, bed="f", sequence="ffb", gauge=1, b
     b1 = sequence[0]
     b2 = "f" if b1 == "b" else "b"
 
-    xfer_loops = {b2: [n for n in list(set(bn_locs[b2]+bed_loops.get(b2, []))) if bnValid(bed, n, gauge) and n not in avoid_bns.get("f", []) and n not in avoid_bns.get("b", []) and n not in secure_needles[b2]]}
+    xfer_loops = {b2: [n for n in list(set(bn_locs.get(b2, [])+bed_loops.get(b2, []))) if bnValid(bed, n, gauge) and n not in avoid_bns.get("f", []) and n not in avoid_bns.get("b", []) and n not in secure_needles[b2]]}
 
     if len(xfer_loops[b2]):
         for n in n_ranges[d2]:
