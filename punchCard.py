a867b70d63b563f118b9bea5d640b89db23e8f6e
gabrielle-ohlson <gmoa2017@mymail.pomona.edu>
2023-10-21 14:52:39 -0400

    punchCard updates (started validate; TODO: finish)

--- punchCard.py
+++ punchCard.py
@@ -1,13 +1,25 @@
+import numpy as np
 import cv2
 # import matplotlib.pyplot as plt
 
-from .helpers import c2cs, toggleDirection, tuckPattern, flattenIter, bnValid
+from .helpers import c2cs, toggleDirection, tuckPattern, flattenIter, bnValid, bnEdges
 
-TUCK = 0
-FAIRISLE = 1
-SLIP = 2
+MAX_FLOAT_WIDTH = 5
 
-def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_dims=None, setting=TUCK, c2=None, color_change_mod=None, gauge=1, gauge_data=False, inhook_carriers=[], outhook_carriers=[]):
+TUCK = 0 # punch card: tuck on non-punched (white) pixels
+FAIRISLE = 1 # punch card: knit color 2 on punched (black) pixels
+SLIP = 2 # punch card: miss on non-punched (white) pixels
+LACE = 3 # punch card: TODO
+
+# The punched hole is the design stitch, with one exception. For tuck stitch it is the UNPUNCHED hole which tucks, so all the other spaces have to be punched (it was what drove me to buy electronic machines).
+
+# needles forward knit next row and needles back tuck
+
+# The punch card holes will be the contrast color in Fair-isle - the top secondary yarn when weaving - the transferred stitch when knitting lace.
+# The punch card holes will be the plain knit stitch when knitting Tuck - and the plain knit stitch when knitting slip/skip.
+
+
+def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_width=24, punch_card_height=None, setting=TUCK, c2=None, color_change_mod=None, gauge=1, gauge_data=False, validate_setting=True, inhook_carriers=[], outhook_carriers=[]):
     '''
     Function to emulate a punch card for a domestic knitting machine on an industrial machine (programmed with knitout) using a binary (black and white) image.
 
@@ -20,12 +32,14 @@ def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_dims=None, 
     * `c` (str or list): the carrier to use for the cast-on (or list of carriers, if plating).
     * `bed` (str, optional): the bed to knit on. Valid values are `f` or `b`.
     * `img_path` (str): path to punch card image.
-    * `punch_card_dims` (tuple or None, optional): dimensions of punch card in (w, h). Defaults to `None`.
+     * `punch_card_width` (int or None, optional): actual width of punch card for resizing image. Defaults to `None` (aka use image width).
+    * `punch_card_height` (int or None, optional): actual height of punch card for resizing image. Defaults to `None` (aka use image height).
     * `setting` (int, optional): program constant specifying the setting to use for "punched out" sections (white pixels). Defaults to `FAIRISLE`.
     * `c2` (str or list, optional): a second carrier (or list of carriers, if plating) to use for color-work (NOTE: required if `setting == FAIRISLE or color_change_mod is not None`). Defaults to `None`.
     * `color_change_mod` (int or None, optional): indicates: "change carriers every `color_change_mod` passes". Defaults to `None`.
     * `gauge` (int, optional): gauge to knit in. Defaults to `1`.
     * `gauge_data` (bool, optional): whether to scale the punch card data to the gauge (so that each pixel is still knitted). Defaults to `False`.
+    * `validate_setting` (bool, optional): whether to check to make sure setting operation followings "knitting rules." Defaults to `True`.
     * `inhook_carriers` (list, optional): carriers to `inhook` before using for the first time (NOTE: will automatically add `tuckPattern` and releasehook too). Defaults to `[]`.
     * `outhook_carriers` (list, optional): carriers to `outhook` at the end of the function. Defaults to `[]`.
 
@@ -38,6 +52,23 @@ def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_dims=None, 
     * (str): next direction to knit carrier `c` in.
     * (str, optional): if `c2 is not None` --- next direction to knit carrier `c2` in.
     '''
+    if setting != TUCK and setting != FAIRISLE and setting != SLIP: raise ValueError(f"Unsupported setting: {setting}.  Supported settings are {TUCK} (tuck), {FAIRISLE} (fairisle), and {SLIP} (slip).")
+    #
+    #get punch card data
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    #
+    if punch_card_width is not None or punch_card_height is not None:
+        punch_card_dims = (punch_card_width if punch_card_width is not None else img.shape[1], punch_card_height if punch_card_height is not None else img.shape[0])
+        img = cv2.resize(img, punch_card_dims, interpolation=cv2.INTER_AREA)
+        # if punch_card_dims is not None: img = cv2.resize(img, punch_card_dims, interpolation=cv2.INTER_AREA)
+    #
+    if setting == FAIRISLE: ret, data = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV) #inverse so that 0 means plain knit with c2
+    else: ret, data = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) # 0 means plain knit
+    #
+    # flip vertically so going from bottom up
+    data = cv2.flip(data, 0)
+    h, w = data.shape
+    #
     directions = {}
     #
     if end_n > start_n: # starts pos
@@ -64,18 +95,6 @@ def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_dims=None, 
         cs2 = None
         if setting == FAIRISLE: raise ValueError("Must pass an argument to 'c2' param for fairisle setting.")
         elif color_change_mod is not None: raise ValueError("Must pass an argument to 'c2' param for color changing.")
-    
-    if setting != TUCK and setting != FAIRISLE and setting != SLIP: raise ValueError(f"Unsupported setting: {setting}.  Supported settings are {TUCK} (tuck), {FAIRISLE} (fairisle), and {SLIP} (slip).")
-    #get punch card data
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    #
-    if punch_card_dims is not None: img = cv2.resize(img, punch_card_dims, interpolation=cv2.INTER_AREA)
-    #
-    ret, data = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-    #
-    # flip vertically so going from bottom up
-    data = cv2.flip(data, 0)
-    h, w = data.shape
     # #remove #debug #v
     # print(data)
     # plt.imshow(data)
@@ -88,13 +107,40 @@ def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_dims=None, 
     else:
         def getData(p, n):
             return data[p%h][n%w]
+    #
+    if validate_setting:
+        print("TODO")
+        if setting == TUCK:
+            left_edge_bn, right_edge_bn = bnEdges(left_n, right_n, gauge, return_type=list)
+            def validate(p, n):
+                if getData(p, n) == 0: return True #not actually a tuck
+                return n != left_edge_bn[1] and n != right_edge_bn[1] and (n-gauge < left_edge_bn[1] or getData(p, n-gauge) == 0) and (n+gauge > right_edge_bn[1] or getData(p, n+gauge) == 0)
+                #
+                """
+                if n == left_edge_bn[1] or n == right_edge_bn[1]: return False
+                if p < passes-1: #another pass is coming
+                    if n-gauge >= left_edge_bn[1] and getData(p, n-gauge) != 0: #tuck on both this needle and needle to the left
+                        if getData(p+1, n) != 0 and getData(p+1, n-gauge) != 0: return False
+                    #
+                    if n+gauge <= right_edge_bn[1] and getData(p, n+gauge) != 0: #tuck on both this needle and needle to the right
+                        if getData(p+1, n) != 0 and getData(p+1, n-gauge) != 0: return False
+                else: return (n-gauge <= left_edge_bn[1] or getData(p, n-gauge) == 0) and (n+gauge >= right_edge_bn[1] or getData(p, n+gauge) == 0) #TODO: maybe have a system to allow some tucks, but just converting it to valid 
+                """
+        else:
+            def validate(p, n):
+                if getData(p, n) == 0: return True #not actually a slip/c2
+                #
+                print("TODO")
+                # kns = np.where(data[p%h] == 0)
+                # if n-gauge < left_edge_bn[1]: left_kn = left_edge_bn[1]
+                # else: left_kn = np.where(data[p%h][:n%w] == 0)             
+    else:
+        validate = lambda p, n: True
 
     #
     do_releasehook = False
     #
     for p in range(passes):
-        if color_change_mod is not None and p % color_change_mod == (color_change_mod-1): cs, cs2 = cs2, cs
-        #
         row = data[p%h] #TODO: #check
         #
         if directions[cs] is None: #inhook
@@ -177,6 +223,8 @@ def generate(k, start_n, end_n, passes, c, bed, img_path, punch_card_dims=None, 
                 do_releasehook = False
             #
             directions[cs2] = toggleDirection(directions[cs2])
+        #
+        if color_change_mod is not None and p % color_change_mod == (color_change_mod-1): cs, cs2 = cs2, cs
     #
     for carrier in flattenIter(outhook_carriers):
         k.outhook(carrier)
